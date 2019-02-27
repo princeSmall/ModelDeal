9497036a9d214a923c2e9a971e4ba209d8bcb003
le tong <1263512924@qq.com>
2019-02-27 17:29:27 +0800

    Initial Commit

---
+++ ModelDeal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelDeal
+//
+//  Created by le tong on 2019/2/27.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelDeal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModelDeal
+//
+//  Created by le tong on 2019/2/27.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
